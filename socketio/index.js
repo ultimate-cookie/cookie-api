6740bfd614446493b47c07dfcb9f5665a61f24cd
Dafe-akaka <dafe.akaka123@gmail.com>
2022-03-14 16:40:41 +0000

    socket connection setup

--- socketio/index.js
+++ socketio/index.js
@@ -1,5 +1,5 @@
 const socketServer = require("./server");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 7000;
 
 socketServer.listen(port, () =>
   console.log(`Socket server is currently running on port: ${port}.`)
