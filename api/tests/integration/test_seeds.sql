e6a07832514006d5f82ab45eb237f075f36bd75d
Dafe-akaka <dafe.akaka123@gmail.com>
2022-03-15 16:30:15 +0000

    test setup

---
+++ api/tests/integration/test_seeds.sql
@@ -0,0 +1,10 @@
+TRUNCATE users RESTART IDENTITY;
+
+
+INSERT INTO users (username, points, game_id)
+VALUES
+    ('saka', 50 , 'test'),
+    ('Auba', 5 , 'test'),
+    ('laca', 57 , 'test'),
+
+    
