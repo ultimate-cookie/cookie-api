ed2c4f88807a26f4e9fb8c258f1b0483d97280e8
Dafe-akaka <dafe.akaka123@gmail.com>
2022-03-15 08:36:31 +0000

    user model all

--- api/model/User.js
+++ api/model/User.js
@@ -1,2 +1,22 @@
-// create model for user db
+const db = require("../db_config/init");
 
+class User {
+  constructor(data) {
+    this.id = data.id;
+    this.username = data.username;
+    this.points = data.points;
+    this.game_id = data.game_id;
+  }
+
+  static get all() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const usersData = await db.query(`SELECT * FROM users;`);
+        const users = usersData.rows.map((u) => new User(u));
+        resolve(users);
+      } catch (err) {
+        reject("Error retrieving Users");
+      }
+    });
+  }
+}
--- db/db.sql
+++ db/db.sql
@@ -3,16 +3,16 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id serial PRIMARY KEY,
     username varchar(250) NOT NULL,
-    lobby_id int NOT NULL
+    points int 
+    game_id int NOT NULL
 );
 
--- DROP TABLE IF EXISTS lobbies; 
+-- DROP TABLE IF EXISTS games; 
 
--- CREATE TABLE lobbies (
+-- CREATE TABLE games (
 --     id serial PRIMARY KEY,
 --     category int NOT NULL,
 --     rounds int NOT NULL,
 --     difficulty varchar(20) NOT NULL  
 -- );
 
--- // talk to group about this
