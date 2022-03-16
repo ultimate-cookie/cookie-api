4a5875d6f7b5d98f9d4ee388e2faeb1a65c55efb
Phil99fp <philipcotter42@gmail.com>
2022-03-16 14:44:22 +0000

    add get request

--- socketio/index.js
+++ socketio/index.js
@@ -15,13 +15,17 @@ const port = process.env.PORT || 7000;
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["*"],
+    origin: ["http://localhost:8080"],
     methods: ["GET", "POST"],
   },
 });
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
 io.on("connection", (socket) => {
-  console.log("the socket is up and running");
+  console.log("New Socket connected: ", socket.id);
   //
   socket.on("joinLobby", ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
@@ -43,23 +47,21 @@ io.on("connection", (socket) => {
   });
 
   // Listen for Questions
-  socket.on;
 
   // Listen for Score
 
   // // socket gets disconnected
   socket.on("disconnect", () => {
-    const user = userLeave(socket.id);
-    if (user) {
-      io.to(user.room).emit("message", `${user.username} has left the room`);
-      console.log("socket disconnected");
-
-      // Send user and room info
-      io.to(user.room).emit("roomUsers", {
-        room: user.room,
-        users: getRoomUsers(user.room),
-      });
-    }
+    // const user = userLeave(socket.id);
+    // if (user) {
+    //   io.to(user.room).emit("message", `${user.username} has left the room`);
+    //   console.log("socket disconnected");
+    //   // Send user and room info
+    //   io.to(user.room).emit("roomUsers", {
+    //     room: user.room,
+    //     users: getRoomUsers(user.room),
+    //   });
+    // }
   });
 });
 
