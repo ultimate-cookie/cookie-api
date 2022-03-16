8593b1927f04e37a031184755be9c661086113e0
Phil99fp <philipcotter42@gmail.com>
2022-03-16 09:30:20 +0000

    working socket port

--- socketio/index.js
+++ socketio/index.js
@@ -1,6 +1,27 @@
-const socketServer = require("./server");
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const { Server } = require("socket.io");
 const port = process.env.PORT || 7000;
 
-socketServer.listen(port, () =>
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["*"],
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("the socket is up and running");
+
+  // // socket gets disconnected
+  // socket.on("disconnect", () => {
+  //   console.log("socket disconnected");
+  // });
+});
+
+httpServer.listen(port, () =>
   console.log(`Socket server is currently running on port: ${port}.`)
 );
